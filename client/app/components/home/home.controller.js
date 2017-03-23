6e3c1b7cb66ec720144fa598a2b9a2ee0d1d352c
yossi <yossitrx@gmail.com>
2017-03-23 17:23:53 +0200

    preparation |  geolocation module

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,24 +1,30 @@
 const STATE = new WeakMap();
-
+const WINDOW = new WeakMap();
 
 class HomeController {
-  constructor(userDataService,$state) {
-    this.name = 'home';
-    this.userDataServiceModuleTest = userDataService;
-    this.userDataServiceModuleTest.getUserData().then((data)=>{
+    constructor(userDataService,$state,$window) {
+        this.name = 'home';
+        this.userDataServiceModuleTest = userDataService;
+        this.userDataServiceModuleTest.getUserData().then((data)=>{
 
-    });
-    STATE.set(this,$state);
-  }
+        });
+        STATE.set(this,$state);
+        WINDOW.set(this,$window);
+    }
 
+    userProfileClick(stateName){
+        STATE.get(this).go(stateName);
+    }
 
-  testClick(stateName){
-    STATE.get(this).go(stateName);
-  }
-
-
+    getLocation() {
+        WINDOW.get(this).navigator.geolocation.getCurrentPosition((position)=>{
+            console.log(position);
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+        });
+    }
 }
 
 
-HomeController.$inject = ['userDataService','$state'];
+HomeController.$inject = ['userDataService','$state','$window'];
 export default HomeController;
