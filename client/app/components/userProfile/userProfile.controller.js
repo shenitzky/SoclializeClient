891ea2fd22837c2ef70ef06fe4c2214f81374293
Yossi <yossitrx@gmail.com>
2017-04-01 23:12:01 +0300

    1. modification to userProfile * cross data userData and factor data. * print and cross data to screen 2. need to complete when user changes selection * using mock data.

--- client/app/components/userProfile/userProfile.controller.js
+++ client/app/components/userProfile/userProfile.controller.js
@@ -1,42 +1,95 @@
 
-const SPORT = 'Sport';
-const GAMEMING = 'Gamming';
-
-
-
 class UserProfileController {
-    constructor(factorsDataService) {
-        this.name = 'userProfile';
-        this.factorsData = factorsDataService;
-    }
-
-    $onInit(){
-        //todo move to const categories
-        this.sport = [];
-        this.gamming = [];
-        this.factorsData.getFactorData().then((data)=>{
-            data = data.data;
-            console.log("data",data);
-            _.forEach(data, (category) => {
-                if(category.class === SPORT){
-                    console.log(category.subClasses);
-                    this.sport.push(category.subClasses)
-                }
-                if(category.class === GAMEMING){
-                    console.log(category.subClasses);
-                    this.gamming.push(category.subClasses);
-                }
-            });
-            console.log("this.sport",this.sport);
-        })
-    }
-
-    onCheckBoxChange(index,category,categoryClass){
-        debugger;
-    }
-
+  constructor(factorsDataService,userDataService) {
+      this.name = 'userProfile';
+      this.userData = userDataService;
+      this.factorsData = factorsDataService;
+  }
+  
+  $onInit(){
+    this.readyTest =false;
+    this.factorsMock =
+      [
+        {
+          'class': 'Sport',
+          'subclasses': [
+            {
+              'name': 'football',
+              'url': './../../app/common/assets/games.png',
+            },
+            {
+              'name': 'tennis',
+              'url': './../../app/common/assets/games.png',
+            }
+          ]
+        },
+        {
+          'class': 'Hobbies',
+          'subclasses': [
+            {
+              'name': 'gaming',
+              'url': './../../app/common/assets/games.png',
+            },
+            {
+              'name': 'ps4',
+              'url': './../../app/common/assets/games.png',
+            }
+          ]
+        }
+      ];
+    //todo i assume that when i getting the data i init it like this mock only then i cross the data.
+    //todo go over the user factor and add to each factor subClass isChosen field true.
+    this.factorsMockForUser =
+      [
+        {
+          'class': 'Sport',
+          'subclasses': [
+            {
+              'name': 'football',
+              'url': './../../app/common/assets/games.png'
+            },
+            {
+              'name': 'tennis',
+              'url': './../../app/common/assets/games.png'
+            }
+          ]
+        }
+      ];
+  
+    this.userMock = {
+      id: "1143afed-6abc-4ef4-b42e-894720979b3a",
+      mail: "xxx@gmail.com",
+      firstName: "Yossi",
+      lastName: "G",
+      description: "Yossi",
+      imgUrl: "",
+      age: 10,
+      premium: false,
+      factors: this.factorsMockForUser
+    };
+    
+    
+    //todo need to imporove this.
+    _.forEach(this.factorsMock , (category)=>{
+      _.forEach(category.subclasses , (subcategory)=>{
+        _.forEach(this.userMock.factors, (userSubCategory)=>{
+          if(_.find(userSubCategory.subclasses,subcategory)){
+            subcategory.isToggle = true;
+          }else {
+            subcategory.isToggle = false;
+          }
+        });
+      });
+    });
+    this.readyTest = true;
+  }
+  
+  onCheckBoxChange(index,category,categoryClass){
+    debugger;
+  }
+  
 }
 
 
-UserProfileController.$inject = ['factorsDataService'];
+UserProfileController.$inject = ['factorsDataService','userDataService'];
 export default UserProfileController;
